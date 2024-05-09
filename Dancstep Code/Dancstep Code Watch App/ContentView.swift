2a4680908cbcf1da4b467363441e256d6a615664
pdancstep <pdancstep@gmail.com>
2024-05-09 12:33:37 -0700

    Created new Apple Watch project

---
+++ Dancstep Code/Dancstep Code Watch App/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Dancstep Code Watch App
+//
+//  Created by PAUL DANCSTEP on 5/9/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Dancstep Code/Dancstep Code Watch App/Dancstep_CodeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Dancstep_CodeApp.swift
+//  Dancstep Code Watch App
+//
+//  Created by PAUL DANCSTEP on 5/9/24.
+//
+
+import SwiftUI
+
+@main
+struct Dancstep_Code_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
